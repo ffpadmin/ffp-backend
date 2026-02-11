539b58062734bfe7218596a5e4733d13728dc6d7
ffpadmin <admin@factfinderspro.com>
2026-02-10 20:52:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI sync from git running!"}
+    return {"message": "FastAPI sync from deploy running!"}
 
 @app.get("/db")
 def test_db():
